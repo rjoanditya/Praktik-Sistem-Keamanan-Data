169367b3f05ff7147db11c6184f7a5089363081b
Rizky Joanditya Nur Iman <85508134+rjoanditya@users.noreply.github.com>
2021-12-24 23:04:19 +0700

    Add files via upload

---
+++ praktikum3/enkcaesar.php
@@ -0,0 +1,26 @@
+<?php
+
+    $kalimat    = $_GET["kata"];
+    $key        = $_GET["key"];
+    for($i=0; $i<strlen($kalimat);$i++){
+        $kode[$i]   = ord($kalimat[$i]); //merubah ASCII ke desimal
+        $b[$i]      = ($kode[$i] + $key) % 256; //proses enkripsi
+        $c[$i]      = chr($b[$i]); // merubah desimal ke ASCII
+    }
+
+    echo "kalimat Asli : ";
+    for($i=0;$i<strlen($kalimat);$i++){
+        echo $kalimat[$i];
+    }
+    echo "<br>";
+    echo "hasil enkripsi : ";
+    $hsl = '';
+    for($i=0;$i<strlen($kalimat);$i++){
+        echo $c[$i];
+        $hsl .= $c[$i];
+    }
+    echo "<br>";
+    //simpan data di file
+    $fp = fopen("enkripsi.txt","w");
+    fputs($fp,$hsl);
+    fclose($fp);
